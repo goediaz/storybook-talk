bead7a240a6f65354f865794ca575e6553ac8895
Rodrigo Diaz <rodrigo.diaz@cyferd.com>
2023-02-13 16:51:47 +0100

    [SB-TALK] Step6

--- src/components/Button.stories.js
+++ src/components/Button.stories.js
@@ -4,6 +4,34 @@ import { action } from '@storybook/addon-actions';
 export default {
   title: 'components/Button',
   component: Button,
+  argTypes: {
+    variant: {
+      options: ['light', 'primary', 'secondary', 'success', 'warning', 'danger'],
+      control: { type: 'select' },
+      description: 'Select a button type to change the color.',
+      table: {
+       defaultValue: { summary: 'light' }
+      }
+    },
+    onClick: {
+      description: 'Callback function to run when the button is clicked', 
+      table: {
+        defaultValue: { summary: 'noop()' }
+      }
+    },
+    label: {
+      control: { type: 'text'},
+      description: 'This is the text thats displayed inside the button',
+      default: false
+    },
+    disabled: {
+      control: { type: 'boolean'},
+      description: 'Toggle if the button should be disabled or not',
+      table: {
+        defaultValue: { summary: false }
+      }
+    }
+  },
 };
 
 const Template = (args) => <Button {...args} />;
