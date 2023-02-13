30ca4d846da50230b67ee30f0fef0c38220555cc
Rodrigo Diaz <rodrigo.diaz@cyferd.com>
2023-02-13 16:47:08 +0100

    [SB-TALK] Step5

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,6 @@ import BSButton from 'react-bootstrap/Button';
 
 export const Button = ({label, variant, disabled, onClick}) => {
   return (
-    <BSButton disabled={disabled} variant={variant} onClick={onClick}>{label}</BSButton>
+    <BSButton disabled={disabled} variant={variant || 'light'} onClick={onClick}>{label}</BSButton>
   );
 }
--- src/components/Button.stories.js
+++ src/components/Button.stories.js
@@ -1,4 +1,5 @@
 import { Button } from './Button';
+import { action } from '@storybook/addon-actions';
 
 export default {
   title: 'components/Button',
@@ -9,7 +10,48 @@ const Template = (args) => <Button {...args} />;
 
 export const Base = Template.bind({});
 Base.args = {
-  onClick: () => alert('Works'),
+  onClick: action('Clicked Base'),
   disabled: false,
+  label: 'Click me!'
+};
+
+export const Primary = Template.bind({});
+Primary.args = {
+  onClick: action('Clicked Primary'),
+  disabled: false,
+  variant: 'primary',
+  label: 'Click me!'
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  onClick: action('Clicked Secondary'),
+  disabled: false,
+  variant: 'secondary',
+  label: 'Click me!'
+};
+
+
+export const Success = Template.bind({});
+Success.args = {
+  onClick: action('Clicked Success'),
+  disabled: false,
+  variant: 'success',
+  label: 'Click me!'
+}
+
+export const Warning = Template.bind({});
+Warning.args = {
+  onClick: action('Clicked Warning'),
+  disabled: false,
+  variant: 'warning',
+  label: 'Click me!'
+}
+
+export const Danger = Template.bind({});
+Danger.args = {
+  onClick: action('Clicked Danger'),
+  disabled: false,
+  variant: 'danger',
   label: 'Click me!'
 };
